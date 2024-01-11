a9ffcc0a8018a916d9b336902be0cabb25757dd0
rbaravikov <robka.bar@gmail.com>
2024-01-11 16:11:04 +0200

    Dar daug trūksta

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,26 +1,29 @@
-import { useState } from "react";
-import { FaTrashAlt, FaEdit } from "react-icons/fa";
+import { FaTrashAlt, FaEdit } from "react-icons/fa"
 
 const Note = ({ notes, setNotes }) => {
   const handleRemove = (index) => {
-    const updatedNotes = [...notes];
-    updatedNotes.splice(index, 1);
-    setNotes(updatedNotes);
-  };
+    const updatedNotes = [...notes]
+    updatedNotes.splice(index, 1)
+    setNotes(updatedNotes)
+  }
+
+  const EnableEdit = ({index, defaultValue}) => {
+    const updateNote = [...notes]
+  }
 
   return (
     <>
       {notes.map((content, index) => (
         <div className="note" key={index}>
           <label htmlFor={index}>
-            <FaEdit />
+            <FaEdit onClick={() => EnableEdit(index, textarea.defaultValue)} />
             <FaTrashAlt onClick={() => handleRemove(index)} />
           </label>
-          <textarea name={index} id={index} value={content} cols="25" rows="15"></textarea>
+          <textarea readOnly name={index} id={index} defaultValue={content} cols="25" rows="15"></textarea>
         </div>
       ))}
     </>
-  );
-};
+  )
+}
 
 export default Note;
